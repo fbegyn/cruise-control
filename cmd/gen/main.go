fbc0ca6c3a172863c63a4b72017414e3123e7cd7
Francis Begyn <francis@begyn.be>
2021-06-15 01:19:11 +0200

    some tests

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"math"
 	"net"
 	"os"
 
@@ -25,13 +26,18 @@ type Config struct {
 	UploadSpeed   float64
 }
 
-func main() {
+type TcConfig struct {
+	Qdiscs  map[string]tc.Object
+	Classes map[string]tc.Object
+	Filters map[string]tc.Object
+}
 
+func main() {
 	// create a context in which the website can run and add logging
 	ctx := opname.With(context.Background(), "main")
 
 	// Enable logging and serve the website
-	ln.Log(ctx, ln.Action("starting_application"))
+	ln.Log(ctx, ln.Action("rendering_config"))
 
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
@@ -45,32 +51,23 @@ func main() {
 	viper.Unmarshal(&conf)
 
 	interfacespeed := 1e9
-	internetspeed := conf.DownloadSpeed * 0.95
-	// routingspeed := interfacespeed * 0.2
+	internetspeed := math.Ceil(conf.DownloadSpeed * 0.95)
+	reservedspeed := math.Ceil(interfacespeed * 0.2)
 
-	prio1speed := internetspeed * 0.4
-	// prio2speed := internetspeed * 0.4
-	// otherspeed := internetspeed * 0.2
+	prio1speed := math.Ceil(internetspeed * 0.4)
+	prio2speed := math.Ceil(internetspeed * 0.4)
+	otherspeed := math.Ceil(internetspeed * 0.2)
 
-	// httpspeed := otherspeed * 0.7
-	// // browserspeed := httpspeed * 0.7
-	// downloadspeed := httpspeed * 0.3
+	httpspeed := math.Ceil(otherspeed * 0.7)
+	browserspeed := math.Ceil(httpspeed * 0.7)
+	downloadspeed := math.Ceil(httpspeed * 0.3)
 
-	// thrashspeed := otherspeed * 0.1
-	// crewspeed := otherspeed * 0.2
+	thrashspeed := math.Ceil(otherspeed * 0.1)
+	crewspeed := math.Ceil(otherspeed * 0.2)
 
 	interf, err := net.InterfaceByName(conf.Interface)
 	if err != nil {
 		ln.FatalErr(ctx, err)
-	}
-
-	defaultHfsc := tc.Attribute{
-		Kind: "hfsc",
-		Hfsc: &tc.Hfsc{
-			Rsc: &tc.ServiceCurve{},
-			Usc: &tc.ServiceCurve{},
-			Fsc: &tc.ServiceCurve{},
-		},
 	}
 
 	ecn := uint32(0)
@@ -88,7 +85,13 @@ func main() {
 		},
 	}
 
-	rootQdisc := tc.Object{
+	template := TcConfig{
+		Qdiscs:  make(map[string]tc.Object),
+		Classes: make(map[string]tc.Object),
+		Filters: make(map[string]tc.Object),
+	}
+
+	template.Qdiscs["root"] = tc.Object{
 		Msg: tc.Msg{
 			Family:  unix.AF_UNSPEC,
 			Ifindex: uint32(interf.Index),
@@ -102,10 +105,74 @@ func main() {
 			},
 			Stab: &tc.Stab{
 				Base: &tc.SizeSpec{
-					MTU: 1500,
+					LinkLayer: 1,
+					MTU:       1500,
 				},
 			},
 		},
+	}
+	template.Qdiscs["prio1"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(11, 0),
+			Parent:  core.BuildHandle(1, 11),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["prio2"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(12, 0),
+			Parent:  core.BuildHandle(1, 12),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["browsing"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(31, 0),
+			Parent:  core.BuildHandle(1, 31),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["dowloading"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(32, 0),
+			Parent:  core.BuildHandle(1, 32),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["thrash"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(22, 0),
+			Parent:  core.BuildHandle(1, 22),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["crew"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(23, 0),
+			Parent:  core.BuildHandle(1, 23),
+		},
+		Attribute: defaultFqCodel,
+	}
+	template.Qdiscs["routing"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(3, 0),
+			Parent:  core.BuildHandle(1, 3),
+		},
+		Attribute: defaultFqCodel,
 	}
 
 	interfaceClass := tc.Object{
@@ -115,48 +182,351 @@ func main() {
 			Handle:  core.BuildHandle(1, 1),
 			Parent:  core.BuildHandle(1, 0),
 		},
-		Attribute: defaultHfsc,
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
 	}
 	SetSC(interfaceClass.Attribute.Hfsc, uint32(interfacespeed), 0, 0)
 	SetUL(interfaceClass.Attribute.Hfsc, uint32(interfacespeed), 0, 0)
+	template.Classes["interface"] = interfaceClass
 
 	internetClass := tc.Object{
-		Msg: tc.Msg{
-			Family:  unix.AF_UNSPEC,
-			Ifindex: uint32(interf.Index),
-			Handle:  core.BuildHandle(1, 11),
-			Parent:  core.BuildHandle(1, 2),
-		},
-		Attribute: defaultHfsc,
-	}
-	SetSC(internetClass.Attribute.Hfsc, uint32(internetspeed), 0, 0)
-	SetUL(interfaceClass.Attribute.Hfsc, uint32(internetspeed), 0, 0)
-
-	prio1Class := tc.Object{
 		Msg: tc.Msg{
 			Family:  unix.AF_UNSPEC,
 			Ifindex: uint32(interf.Index),
 			Handle:  core.BuildHandle(1, 2),
 			Parent:  core.BuildHandle(1, 1),
 		},
-		Attribute: defaultHfsc,
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
 	}
-	SetSC(prio1Class.Attribute.Hfsc, uint32(prio1speed), 0, 0)
+	SetSC(internetClass.Attribute.Hfsc, uint32(internetspeed), 0, 0)
+	SetUL(interfaceClass.Attribute.Hfsc, uint32(internetspeed), 0, 0)
+	template.Classes["internet"] = internetClass
 
-	prio1Qdisc := tc.Object{
+	prio1Class := tc.Object{
 		Msg: tc.Msg{
 			Family:  unix.AF_UNSPEC,
 			Ifindex: uint32(interf.Index),
-			Handle:  core.BuildHandle(11, 0),
-			Parent:  core.BuildHandle(1, 11),
+			Handle:  core.BuildHandle(1, 21),
+			Parent:  core.BuildHandle(1, 2),
 		},
-		Attribute: defaultFqCodel,
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetSC(prio1Class.Attribute.Hfsc, uint32(prio1speed), 0, 0)
+	template.Classes["prio1"] = prio1Class
+
+	prio2Class := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 22),
+			Parent:  core.BuildHandle(1, 2),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetSC(prio2Class.Attribute.Hfsc, uint32(prio2speed), 60000, 0)
+	template.Classes["prio2"] = prio2Class
+
+	otherClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 23),
+			Parent:  core.BuildHandle(1, 2),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(otherClass.Attribute.Hfsc, uint32(otherspeed), 100000, 0)
+	template.Classes["other"] = otherClass
+
+	httpClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 231),
+			Parent:  core.BuildHandle(1, 23),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(httpClass.Attribute.Hfsc, uint32(httpspeed), 0, 0)
+	template.Classes["http"] = httpClass
+
+	browseClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 2311),
+			Parent:  core.BuildHandle(1, 231),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetSC(browseClass.Attribute.Hfsc, uint32(browserspeed), 0, 0)
+	template.Classes["browse"] = browseClass
+
+	downloadClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 2312),
+			Parent:  core.BuildHandle(1, 231),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(downloadClass.Attribute.Hfsc, uint32(downloadspeed), 10000, 0)
+	template.Classes["download"] = downloadClass
+
+	crewClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 232),
+			Parent:  core.BuildHandle(1, 23),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(crewClass.Attribute.Hfsc, uint32(crewspeed), 0, 0)
+	template.Classes["crew"] = crewClass
+
+	thrashClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 233),
+			Parent:  core.BuildHandle(1, 23),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(thrashClass.Attribute.Hfsc, uint32(thrashspeed), 50000, 0)
+	template.Classes["thrash"] = thrashClass
+
+	reservedClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(1, 3),
+			Parent:  core.BuildHandle(1, 1),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{},
+				Usc: &tc.ServiceCurve{},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	SetLS(reservedClass.Attribute.Hfsc, uint32(reservedspeed), 0, 0)
+	template.Classes["reserved"] = reservedClass
+
+	prio1Handle := template.Classes["prio1"].Msg.Handle
+	template.Filters["prio1"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &prio1Handle,
+				Mark: &tc.U32Mark{
+					Val:  0x1,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	prio2Handle := template.Classes["prio2"].Msg.Handle
+	template.Filters["prio2"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &prio2Handle,
+				Mark: &tc.U32Mark{
+					Val:  0x2,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	browseHandle := template.Classes["browse"].Msg.Handle
+	template.Filters["other"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &browseHandle,
+				Mark: &tc.U32Mark{
+					Val:  0x3,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	downloadHandle := template.Classes["download"].Msg.Handle
+	template.Filters["download"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &downloadHandle,
+				Mark: &tc.U32Mark{
+					Val:  0x4,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	crewHandle := template.Classes["crew"].Msg.Handle
+	template.Filters["crew"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &crewHandle,
+				Mark: &tc.U32Mark{
+					Val:  0x5,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	thrashHandle := template.Classes["thrash"].Msg.Handle
+	template.Filters["thrash"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &thrashHandle,
+				Mark: &tc.U32Mark{
+					Val:  0x6,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+	reservedHandle := template.Classes["reserved"].Msg.Handle
+	template.Filters["reserved"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "fw",
+			Fw: &tc.Fw{
+				ClassID: &reservedHandle,
+				InDev:   &conf.Interface,
+			},
+		},
+	}
+	template.Filters["arp"] = tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(1, 0),
+		},
+		Attribute: tc.Attribute{
+			Kind: "fw",
+			Fw: &tc.Fw{
+				ClassID: &reservedHandle,
+				InDev:   &conf.Interface,
+			},
+		},
 	}
 
-	qdiscs := []*tc.Object{}
-	qdiscs = append(qdiscs, &rootQdisc)
-	qdiscs = append(qdiscs, &prio1Qdisc)
-
-	b, _ := json.Marshal(qdiscs)
+	b, err := json.Marshal(template)
+	if err != nil {
+		ln.FatalErr(ctx, err)
+	}
 	os.Stdout.Write(b)
 }
