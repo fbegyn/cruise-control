1494e3f1ee4a4dbcd3a70b354fc8197153626f36
Francis Begyn <francis@begyn.be>
2021-12-22 16:58:45 +0100

    intermediate debugging commit

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -2,26 +2,111 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"strings"
 
+	"github.com/florianl/go-tc"
+	"github.com/florianl/go-tc/core"
+
 	"github.com/mdlayher/netlink"
+	"golang.org/x/sys/unix"
 )
 
 const nlafNested = (1 << 15)
 
 func main() {
-	tests := map[string][]byte{
-		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=92, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_EXCL|NLM_F_CREATE, nlmsg_seq=1625691781, nlmsg_pid=0}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=768}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=48, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x01,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x11,0x00,0x01,0x00,0x14,0x00,0x05,0x00,0x01,0x00,0x00,0x00"...}]}, iov_len=92}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 92
-		"iproute": []byte{0x10, 0x00, 0x0a, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x11, 0x00, 0x01, 0x00, 0x14, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00},
-		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=72, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_CREATE, nlmsg_seq=563752620, nlmsg_pid=28466}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=0}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=28, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x05,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x23,0x00,0x01,0x00"}]}, iov_len=72}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 72
-		"app": []byte{0x10, 0x00, 0x0a, 0x00, 0x05, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x23, 0x00, 0x01, 0x00},
+	interf, err := net.InterfaceByName("test-01")
+	if err != nil {
+		panic(err)
 	}
 
-	for key, value := range tests {
-		fmt.Println(key)
-		decodeNetlink(value, 2)
+	// open up tc socker for the test interface
+	rtnl, err := tc.Open(&tc.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = rtnl.SetOption(netlink.ExtendedAcknowledge, true)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := rtnl.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// add the HFSC qdisc to the root ffff:0000
+	testQdisc := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(0xFFFF, 0x0000),
+			Parent:  tc.HandleRoot,
+			Info:    0,
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			HfscQOpt: &tc.HfscQOpt{
+				DefCls: 1,
+			},
+		},
+	}
+	err = rtnl.Qdisc().Replace(&testQdisc)
+	if err != nil {
+		panic(err)
+	}
+
+	// provide a class for HFSC classid: ffff:0001
+	internetClass := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Handle:  core.BuildHandle(0xFFFF, 0x0001),
+			Parent:  core.BuildHandle(0xFFFF, 0x0000),
+		},
+		Attribute: tc.Attribute{
+			Kind: "hfsc",
+			Hfsc: &tc.Hfsc{
+				Rsc: &tc.ServiceCurve{100e6, 0, 0},
+				Usc: &tc.ServiceCurve{100e6, 0, 0},
+				Fsc: &tc.ServiceCurve{},
+			},
+		},
+	}
+	err = rtnl.Class().Replace(&internetClass)
+	if err != nil {
+		panic(err)
+	}
+
+	// set a filter for match traffic to the internetclass
+	testFilter := tc.Object{
+		Msg: tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(interf.Index),
+			Parent:  core.BuildHandle(0xFFFF, 0x0000),
+			Info:    0,
+		},
+		Attribute: tc.Attribute{
+			Kind: "u32",
+			U32: &tc.U32{
+				ClassID: &internetClass.Msg.Handle,
+				Mark: &tc.U32Mark{
+					Val:  0x6,
+					Mask: 0xf,
+				},
+			},
+		},
+	}
+
+	err = rtnl.Filter().Add(&testFilter)
+	if err != nil {
+		panic(err)
 	}
 }
+
+// notes:
+// @florian: and app sets also TCA_U32_UNSPEC (0) which should be TCA_U32_CLASSID (1) instead, I guess
+//           setting xx_UNSPEC always returns an invalid argument
 
 func decodeNetlink(data []byte, lvl int) error {
 	ad, err := netlink.NewAttributeDecoder(data)
@@ -31,6 +116,10 @@ func decodeNetlink(data []byte, lvl int) error {
 	for ad.Next() {
 		space := strings.Repeat(" ", lvl)
 		if ad.Type()&nlafNested == nlafNested {
+			// The data is formatted in the following structure
+			// <number> - <byte data>
+			// <number>: types from the enum struct
+			// <byte data>: byte values for this struct
 			fmt.Printf("%s %4d | %d\n", space, ad.Type(), ad.Type()^nlafNested)
 			decodeNetlink(ad.Bytes(), lvl+3)
 			continue
@@ -39,3 +128,17 @@ func decodeNetlink(data []byte, lvl int) error {
 	}
 	return nil
 }
+
+//	// Here we need to define the netlink data from TCA_options. Later it is then possible
+//	// to lookup the output in the linux kernel docs structs for the things.
+//	tests := map[string][]byte{
+//		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=92, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_EXCL|NLM_F_CREATE, nlmsg_seq=1625691781, nlmsg_pid=0}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=768}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=48, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x01,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x11,0x00,0x01,0x00,0x14,0x00,0x05,0x00,0x01,0x00,0x00,0x00"...}]}, iov_len=92}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 92
+//		"iproute": []byte{0x10, 0x00, 0x0a, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x11, 0x00, 0x01, 0x00, 0x14, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00},
+//		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=72, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_CREATE, nlmsg_seq=563752620, nlmsg_pid=28466}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=0}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=28, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x05,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x23,0x00,0x01,0x00"}]}, iov_len=72}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 72
+//		"app": []byte{0x10, 0x00, 0x0a, 0x00, 0x05, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x23, 0x00, 0x01, 0x00},
+//	}
+//
+//	for key, value := range tests {
+//		fmt.Println(key)
+//		decodeNetlink(value, 2)
+//	}
--- go.mod
+++ go.mod
@@ -1,24 +1,40 @@
 module github.com/fbegyn/cruise-control
 
-go 1.15
+go 1.17
 
 replace github.com/florianl/go-tc => /home/francis/go/src/github.com/fbegyn/go-tc
 
 require (
-	github.com/florianl/go-tc v0.3.0
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/go-kit/kit v0.10.0
+	github.com/florianl/go-tc v0.3.1-0.20211218153208-f8d5954ba613
+	github.com/mdlayher/netlink v1.5.0
+	github.com/spf13/viper v1.10.1
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486
+	within.website/ln v0.9.1
+)
+
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/josharian/native v0.0.0-20200817173448-b6b71def0850 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mdlayher/netlink v1.4.1 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/pelletier/go-toml v1.9.1 // indirect
+	github.com/mdlayher/socket v0.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.7.1
-	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	within.website/ln v0.9.1 // indirect
+	honnef.co/go/tools v0.2.2 // indirect
 )
