096ee18c77db7c82625cb7df7e2ba7f000faf824
Francis Begyn <francis@begyn.be>
2021-12-22 20:49:00 +0100

    working debug settings

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -84,21 +84,22 @@ func main() {
 			Family:  unix.AF_UNSPEC,
 			Ifindex: uint32(interf.Index),
 			Parent:  core.BuildHandle(0xFFFF, 0x0000),
-			Info:    0,
+			Info:    768,
 		},
 		Attribute: tc.Attribute{
 			Kind: "u32",
 			U32: &tc.U32{
 				ClassID: &internetClass.Msg.Handle,
+				Sel:     &tc.U32Sel{},
 				Mark: &tc.U32Mark{
-					Val:  0x6,
+					Val:  0x1,
 					Mask: 0xf,
 				},
 			},
 		},
 	}
 
-	err = rtnl.Filter().Add(&testFilter)
+	err = rtnl.Filter().Replace(&testFilter)
 	if err != nil {
 		panic(err)
 	}
@@ -129,16 +130,18 @@ func decodeNetlink(data []byte, lvl int) error {
 	return nil
 }
 
-//	// Here we need to define the netlink data from TCA_options. Later it is then possible
-//	// to lookup the output in the linux kernel docs structs for the things.
-//	tests := map[string][]byte{
-//		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=92, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_EXCL|NLM_F_CREATE, nlmsg_seq=1625691781, nlmsg_pid=0}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=768}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=48, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x01,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x11,0x00,0x01,0x00,0x14,0x00,0x05,0x00,0x01,0x00,0x00,0x00"...}]}, iov_len=92}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 92
-//		"iproute": []byte{0x10, 0x00, 0x0a, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x11, 0x00, 0x01, 0x00, 0x14, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00},
-//		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=72, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_CREATE, nlmsg_seq=563752620, nlmsg_pid=28466}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=0}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=28, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x05,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x23,0x00,0x01,0x00"}]}, iov_len=72}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 72
-//		"app": []byte{0x10, 0x00, 0x0a, 0x00, 0x05, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x23, 0x00, 0x01, 0x00},
-//	}
-//
-//	for key, value := range tests {
-//		fmt.Println(key)
-//		decodeNetlink(value, 2)
-//	}
+func debug() {
+	// Here we need to define the netlink data from TCA_options. Later it is then possible
+	// to lookup the output in the linux kernel docs structs for the things.
+	tests := map[string][]byte{
+		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base=[{nlmsg_len=92, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_EXCL|NLM_F_CREATE, nlmsg_seq=1640201947, nlmsg_pid=0}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=768}, [[{nla_len=8, nla_type=TCA_KIND}, "u32"], [{nla_len=48, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x01,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x11,0x00,0x01,0x00,0x14,0x00,0x05,0x00,0x01,0x00,0x00,0x00"...]]], iov_len=92}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 92
+		"iproute": []byte{0x10, 0x00, 0x0a, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x11, 0x00, 0x01, 0x00, 0x14, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00},
+		// sendmsg(3, {msg_name={sa_family=AF_NETLINK, nl_pid=0, nl_groups=00000000}, msg_namelen=12, msg_iov=[{iov_base={{nlmsg_len=72, nlmsg_type=RTM_NEWTFILTER, nlmsg_flags=NLM_F_REQUEST|NLM_F_ACK|NLM_F_CREATE, nlmsg_seq=563752620, nlmsg_pid=28466}, {tcm_family=AF_UNSPEC, tcm_ifindex=if_nametoindex("test-01"), tcm_handle=0, tcm_parent=65536, tcm_info=0}, [{{nla_len=8, nla_type=TCA_KIND}, "u32"}, {{nla_len=28, nla_type=TCA_OPTIONS}, ",0x10,0x00,0x0a,0x00,0x05,0x00,0x00,0x00,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x01,0x00,0x23,0x00,0x01,0x00"}]}, iov_len=72}], msg_iovlen=1, msg_controllen=0, msg_flags=0}, 0) = 72
+		"app": []byte{0x10, 0x00, 0x0a, 0x00, 0x05, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x01, 0x00, 0x23, 0x00, 0x01, 0x00},
+	}
+
+	for key, value := range tests {
+		fmt.Println(key)
+		decodeNetlink(value, 2)
+	}
+}
