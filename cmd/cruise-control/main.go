8617a79c677eb47f8a7d18c3e267968d74f4a3f2
Francis Begyn <francis@begyn.be>
2023-10-18 02:31:36 +0200

    enough work done

--- cmd/cruise-control/main.go
+++ cmd/cruise-control/main.go
@@ -10,10 +10,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/florianl/go-tc"
-	"github.com/florianl/go-tc/core"
 	"github.com/mdlayher/netlink"
 	"github.com/spf13/viper"
 	"golang.org/x/sys/unix"
@@ -112,101 +110,59 @@ func ObjectCreateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("Cruise control updated"))
 
-	fmt.Println(data)
-}
-
-func TCApplyHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := opname.With(context.Background(), "TCApplyHandler")
-	devName := r.URL.Query().Get("interface")
-	speed, err := strconv.Atoi(r.URL.Query().Get("up"))
-	if err != nil {
-		ln.FatalErr(ctx, err)
-	}
-	ln.Log(ctx, ln.Info("interface: %s - speed: %d Mbps", devName, speed))
-
-	interf, err := net.InterfaceByName(devName)
-	tcConf := createQoSSimple(ctx, *interf, 1e9, speed)
-
-	// construct the TC nodes from the config file
-	var nodes []*Node
-	for _, class := range tcConf.Classes {
-		n := NewNodeWithObject("class", class)
-		nodes = append(nodes, n)
-	}
-	for _, qdisc := range tcConf.Qdiscs {
-		n := NewNodeWithObject("qdisc", qdisc)
-		nodes = append(nodes, n)
-	}
-
-	var filters []*Node
-	for _, filter := range tcConf.Filters {
-		n := NewNodeWithObject("filter", filter)
-		filters = append(filters, n)
-	}
-
-	// construct the TC tree
-	tree, index := FindRootNode(nodes)
-	if tree == nil {
-		ln.FatalErr(ctx, err)
-	}
-	nodes = append(nodes[:index], nodes[index+1:]...)
-	nodes = tree.ComposeChildren(nodes)
-	if len(nodes) == 0 {
-		ln.Log(ctx, ln.Info("all TC nodes parsed, tree constructed"))
-	} else {
-		ln.Log(ctx, ln.Info("there are leftover TC nodes: %d nodes left", len(nodes)))
-	}
 
 	// open a go-tc socket
 	rtnl, err := tc.Open(&tc.Config{})
 	if err != nil {
-		ln.FatalErr(ctx, err)
+		logger.Error("failed to open rtnetlink socket", "err", err)
 		return
 	}
 	err = rtnl.SetOption(netlink.ExtendedAcknowledge, true)
 	if err != nil {
-		ln.FatalErr(ctx, err)
+		logger.Error("failed to set extended acknowledge", "err", err)
 		return
 	}
 	defer func() {
 		if err := rtnl.Close(); err != nil {
-			ln.FatalErr(ctx, err)
+		logger.Error("failed to close rtnetlink socket", "err", err)
 		}
 	}()
+	logger.Info("opened rtnetlink socket")
 
-	// get the system tree and compare it to the current config. If there is a difference, we should
-	// reapply the tree so the config is matched
-	ln.Log(ctx, ln.Action("Fetching current TC state"))
-	systemNodes, systemFilters := GetInterfaceNodes(rtnl, uint32(interf.Index))
-	systemTree, index := FindRootNode(systemNodes)
-	systemNodes = append(systemNodes[:index], systemNodes[index+1:]...)
-	systemTree.ComposeChildren(systemNodes)
-
-	// check if the system is up to date or not
-	if !systemTree.CompareTree(*tree) {
-		ln.Log(ctx, ln.Info("updating the current interfaces qdiscs and classes"))
-		tree.ApplyNode(rtnl)
-	} else {
-		ln.Log(ctx, ln.Info("current interface is already up to date with the qdiscs and classes"))
-	}
-
-	test := make(map[uint32]struct{})
-	for _, t := range systemFilters {
-		if _, ok := test[t.Object.Handle]; ok {
+	for _, d := range data {
+		obj, err := JSONToTC(d)
+		if err != nil {
+			logger.Error("failed to convert JSON into TC obj", "err", err)
 			continue
 		}
-		test[t.Object.Handle] = struct{}{}
-	}
+		logger.Info(
+			"generated TC object",
+			"handle", d.Handle,
+			"parent", d.Parent,
+			"kind", obj.Attribute.Kind,
+			"type", d.Type,
+			"interface", d.Interface,
+			"ifIndex", obj.Ifindex,
+		)
 
-	ln.Log(ctx, ln.Action("Applying filters"))
-	for _, filt := range filters {
-		if _, k := test[filt.Object.Handle]; k {
-			fmt.Println("fitler found")
+		switch d.Type {
+		case "qdisc":
+			err = rtnl.Qdisc().Replace(&obj)
+			if err != nil {
+				logger.Error("failed to replace qdisc", "handle", d.Handle, "err", err)
+			}
+		case "class":
+			err = rtnl.Class().Replace(&obj)
+			if err != nil {
+				logger.Error("failed to replace class", "handle", d.Handle, "err", err)
+			}
+		case "filter":
+			err = rtnl.Filter().Replace(&obj)
+			if err != nil {
+				logger.Error("failed to replace filter", "handle", d.Handle, "err", err)
+			}
+		default:
+			logger.Warn("unkown TC object type", "type", d.Type)
 		}
-		filt.ApplyNode(rtnl)
 	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/text")
-	w.Write([]byte("Cruise control updated"))
 }
